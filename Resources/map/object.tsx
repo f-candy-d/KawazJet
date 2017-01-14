d93c11e66b104f1db6896adb88a9abadf83a92eb
f-candy-d <pagunouetamuramaro@gmail.com>
2017-01-14 12:17:30 +0900

    first commit!

---
+++ Resources/map/object.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="object" tilewidth="24" tileheight="24">
+ <image source="object.png" width="120" height="24"/>
+ <tile id="0">
+  <properties>
+   <property name="category" value="4"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="category" value="8"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="category" value="8"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="category" value="4"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="category" value="4"/>
+  </properties>
+ </tile>
+</tileset>
